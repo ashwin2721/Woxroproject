8408ffdf3bcbcd589333d299a0fd9f5047f3705a
ashwin2721 <136354229+ashwin2721@users.noreply.github.com>
2024-02-02 11:34:57 +0530

    banner.js file inside the component

---
+++ Banner.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./Banner.css"
+function Banner() {
+  return (
+    <div className='banner'>
+        <div className='content'>
+        <h1 className='description'>Create, play, and battle with friends for free in Fortnite. Be the last player standing in Battle Royale and Zero Build, experience a concert or live event, or discover over a million creator made games, including racing, parkour, zombie survival, and more. Each Fortnite island has an individual age rating so you can find the one that's right for you and your friends. Find it all in Fortnite ... Drop In.
+         <div>
+         <img src="https://s3-alpha-sig.figma.com/img/e623/6bd7/1dd8e61fcac2c9c7af98f1fbbf93d497?Expires=1707696000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jqym85cLEJ7T5~k~Z4g5Z1kgIGpd-Rmlvsv-uYLaTo2JKGvz-x24XGLHs8wIngtbcsbIbTCj6p0zInpkQMIWffRpc0nl~lpYcB45l2eeGo8QdowA2pVRWPkZ0hAi2MSOlGz-TQBBtqbUZosNvjrfJbg-aQfXG3eNDdvUHLLUy42KhHkvn6G6gucID0tDbEA9fgKD-xnvgqoXGTeDT~awhcJA73PFVqGGzGFgqFJLN8KBaZ7U220cKVutrmbwZj0TuOKqoQsNYj0M6BkaSwdHzCgYAUGGsGwmts1M5-gCTLZUVGHjHdg2RjkZfnhcnxIqa8QLccF17HHgVpeDphK0wQ__" alt="ttt" />
+         </div>
+      <div className='banner-but'>
+        <button className='button'>visit website</button>
+        
+
+      </div>
+      
+</h1>
+      </div>
+      <div className="fade"></div> 
+    </div>
+  )
+}
+
+export default Banner
