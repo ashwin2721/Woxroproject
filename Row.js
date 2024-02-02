05fd0b5025e973fa72876b49a4fb9492e467d62f
ashwin2721 <136354229+ashwin2721@users.noreply.github.com>
2024-02-02 11:35:50 +0530

    row.js file inside the component

---
+++ Row.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Row.css'
+function Row() {
+  return (
+    <div>
+      
+      <div className='poster'>
+        <img src="https://s3-alpha-sig.figma.com/img/7147/9344/2501faa7f289434a9b99bafaa474cc0e?Expires=1707696000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=eH~wuXAEILf-vKik9f6z0TaJy-rKIilEDMS~j6ZZQoMfgaznxD8FQLah5BlsayxPfP8yLiJSwwuhyndVSS6eq~rDRUODS1qB4HhMxgczcV55xB4cJ10a1dVc0eAHjxwsNJBEDM7w3zcNM6Y5Z8oLsCxNQM3~rPbeuwAUhDkvEAyzGAfvoT6mAvi46dh~OPc8fbz2Z7j9m4MuKZ9~Ca-6sZktJOzdf32O4zcwErgx1otoaYtGejZbQxE-MHdB3GyIf~GvKbeIrztYLd2OZH5zYcsee0Z7c0KeGMpYkAqIBVSo~n3hZOgBmxzsmvmAbf7wvOBsLSD72wVDgLPA~6hplw__" alt="ttt" />
+        <h2>Explore large, destructible environments where no two games are ever the same.</h2>
+        <img src="https://s3-alpha-sig.figma.com/img/ff28/c642/d7258eeaa99e5ea5585055099ce474ea?Expires=1707696000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=PjWMSRRHXYUv3UkNpEW2Cj1AYamttGy5MOc9jzQvIY5juhqO~I4TtHrhfwsnGx9ZvfYzwp5K34RRjBpBrqPSwGIgyH8Ty5dEpHsuiwCPqEq3tkozfVrmweX3iyftWgNqnFJEZofuIva4H7YeioPgIsmttFWsBIasi-E9OYNSaC49oqOl2~ofU-3Xrapf67Tgg4K93ZN5Z6XMiBjfi2sHbNF0r8NPRnA9fEMruVcSJHZFwKkZuCXq4ER2h7P2aweyVDrYV5vIEB~Ben8dvEuntBn2hMXeqIyYdTIGc7qJxMfLfTnATZM~OYBCk4CIkuS0HxWy~R5USlJlc-SNO9GPFw__" alt="jjj" />
+<h3>Team up with friends by sprinting, climbing and smashing your way to earn your Victory Royale</h3>
+        <img src="https://s3-alpha-sig.figma.com/img/a09a/b5b0/95547eb344eb0c0875a5194c38c5747c?Expires=1707696000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=MAjdRF4TPncO4RRFu38vfCl9Oqv1VcH5S1Tv5-nDzcDqXNc2R34~zuoe7e6u6nM~N5cBnVtvCu3M2QRi3mequyUJnyjbi-DnF~rGFj71MqPsFBgJMF~sDXrRjOwzKUmnJqqt9v~2jEGXXXyLbjMjNhBDe~-zeIy9NlGoRMuP9-gYsw2auC-TX-xfo-~xFJ2KJVKiXKFCrNyi-o~M1w~itFgmrfoI3~1M1nvJTD~1ziz~oaxHof-tRkN5VJuZ5MKFhA2Ew3aU0IPzSYNQ3ykxKlTST-YQtBLsG-KQhZXpTDT8WrIIEA1rjjUzqyaUtiduX1nUFQqyCj6MDsTVp4kW5g__" alt="ppp" />
+        <h4>Discover even more ways to play across thousands of creator-made game genres</h4>
+      </div>
+
+    </div>
+  )
+}
+
+export default Row
