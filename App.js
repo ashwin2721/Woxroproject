30ce8238319410ee4666887e70cd98f47f023f91
ashwin2721 <136354229+ashwin2721@users.noreply.github.com>
2024-02-02 11:31:48 +0530

    code of page jsfile

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Banner from './component/banner/Banner';
+import Row from './component/Row/Row';
+
+function App() {
+  return (
+    <div>
+   
+    <Banner/>
+    <Row/>
+    </div>
+  )
+}
+
+export default App
